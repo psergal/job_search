d17e08ceeafa1360762bb7a55a870b495aedfb44
Pserga <psergal@gmail.com>
2019-06-02 19:09:07 +0300

    First working version with GAP in JS

--- arc/regexpjson.py
+++ arc/regexpjson.py
@@ -1,21 +1,25 @@
 import json
 import re
 # 1С  по русски
-pop_jobs = ['TypeScript', 'Swift', 'Scala', 'Objective-C', 'Shell', 'Go', 'C#', 'C\+\+', '1С', 'C', 'PHP', 'Ruby', 'Python', 'Java', 'JavaScript']
+pop_jobs = ['TypeScript', 'Swift', 'Scala', 'Objective-C', 'Shell', 'Go', 'C#', 'C++', '1С', 'C', 'PHP', 'Ruby', 'Python', 'Java', 'JavaScript']
+# pop_jobs = ['C#', 'C++', '1С', 'C']
 pop_jobs = {job: 0 for job in pop_jobs}
 
 with open('hh1.json', 'r',encoding='utf8') as hh:
     sfile = json.load(hh)
 
 for item in sfile.get('items'):
-    print(item.get('id'),item.get('name'))
+    # print(item.get('id'),item.get('name'))
+    Match = False
     for job, count in pop_jobs.items():
-        regex = re.compile(r'\b'+job, re.IGNORECASE)
+        pattern = re.sub(r'C\+\+', 'C\+\+', job)
+        regex = re.compile(pattern, re.IGNORECASE)
         search_str= re.sub('1C', '1С', item.get('name'))
         search_str= re.sub(r'С\+{2}', 'C++', search_str)
         search_str= re.sub(r'С\#', 'C#', search_str)
         match = re.search(regex, search_str)
         if match:
             pop_jobs[job] = count+1
-            print(regex,'---', job, pop_jobs[job])
-
+            # print(regex,'---', job, pop_jobs[job])
+            break
+print(pop_jobs)
--- seek_job.py
+++ seek_job.py
@@ -1,6 +1,6 @@
 import requests
 import json
-
+import re
 
 def look_for_job():
     job_api = 'https://api.hh.ru'
@@ -15,13 +15,25 @@ def look_for_job():
     p = 'period'
     id = '29443519'
     spec_key = '?specialization=Банковское ПО'
+    pop_languages = ['TypeScript', 'Swift', 'Scala', 'Kotlin', 'Go', 'C#',
+                'C++', '1С', 'C', 'PHP', 'Ruby', 'Python', 'Java', 'JavaScript']
+    pop_languages = {lang: 0 for lang in pop_languages}
     # resp = requests.get(f'{job_api}/{v}/{id}', headers=headers)
-    resp = requests.get(f'{job_api}/{v}?{s}=Программист&{a}=1&{p}=30&page=1&per_page=20', headers=headers)
-    with open("hh1.json", "w",encoding='utf8') as write_file:
-        json.dump(resp.json(), write_file, indent=2, ensure_ascii=False)
-    # print(resp.json())
-
-
+    resp = requests.get(f'{job_api}/{v}?{s}=Программист&{a}=1&{p}=30&page=0&per_page=20', headers=headers)
+    for page in range(resp.json().get('pages')):
+        resp = requests.get(f'{job_api}/{v}?{s}=Программист&{a}=1&{p}=30&page={page}&per_page=20', headers=headers)
+        for item in resp.json().get('items'):
+            for lang, count in pop_languages.items():
+                pattern = re.sub(r'C\+\+', 'C\+\+', lang)
+                regex = re.compile(pattern, re.IGNORECASE)
+                search_str = re.sub('1C', '1С', item.get('name'))
+                search_str = re.sub(r'С\+{2}', 'C++', search_str)
+                search_str = re.sub(r'С\#', 'C#', search_str)
+                match = re.search(regex, search_str)
+                if match:
+                    pop_languages[lang] = count+1
+                    break
+    print(pop_languages)
 
 if __name__ == '__main__':
     look_for_job()
